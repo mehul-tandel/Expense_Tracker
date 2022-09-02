89ec039a7f9e20c9e5e753ab3e3617335ec0e6cc
Mehul Tandel <mehultandel.tech@gmail.com>
2022-09-02 16:50:33 +0530

    Expenses component seperated from App

---
+++ src/components/Expenses.js
@@ -0,0 +1,33 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css"
+
+const Expenses = (props) => {
+  const exp = props.items;
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={exp[0].title}
+        amount={exp[0].amount}
+        date={exp[0].date}
+      />
+      <ExpenseItem
+        title={exp[1].title}
+        amount={exp[1].amount}
+        date={exp[1].date}
+      />
+      <ExpenseItem
+        title={exp[2].title}
+        amount={exp[2].amount}
+        date={exp[2].date}
+      />
+      <ExpenseItem
+        title={exp[3].title}
+        amount={exp[3].amount}
+        date={exp[3].date}
+      />
+      
+    </div>
+  );
+};
+
+export default Expenses;
