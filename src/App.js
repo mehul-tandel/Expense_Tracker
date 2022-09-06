59499df0e761add35bdc78b1a181e02af22a16ac
Mehul Tandel <mehultandel.tech@gmail.com>
2022-09-06 22:23:19 +0530

    toggle functionality to add new expense

--- src/App.js
+++ src/App.js
@@ -43,7 +43,6 @@ const App = () => {
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
-      {console.log(expenses)};
     </div>
   );
 };
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [toggle, setToggle] = useState(false);
+
   const onSaveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
-    console.log(expenseData);
     props.onAddExpense(expenseData);
+    setToggle(false);
   };
+
+  const cancelButtonHandler = () => setToggle(false);
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
+      {!toggle && (
+        <button onClick={() => setToggle(true)}>Add New Expense</button>
+      )}
+      {toggle && (
+        <ExpenseForm
+          onSaveExpenseData={onSaveExpenseDataHandler}
+          onCancel={cancelButtonHandler}
+        />
+      )}
     </div>
   );
 };
